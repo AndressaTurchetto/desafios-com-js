bff82678b6dbb83382ccd433cfe4d3a3a14a4b6d
Bowie <145608228+AndressaTurchetto@users.noreply.github.com>
2024-01-19 16:37:51 -0200

    Atualização do exercício

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
-if (numeroSecreto == chute) {
-    console.log('Você acertou o número secreto! (5)')
+if (chute == numeroSecreto) {
+    alert(`Você acertou o numero secreto  ${numeroSecreto}`)
+} else {
+    alert('Você errou! Tente novamente!')
 }
