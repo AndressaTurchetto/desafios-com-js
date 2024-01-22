d3e1ce1a41797aea32bdbb4c76cfd5083d88bce9
Bowie <145608228+AndressaTurchetto@users.noreply.github.com>
2024-01-22 20:32:12 -0200

    Atualização function

--- ex002/app.js
+++ ex002/app.js
@@ -1,5 +1,27 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//Aqui abaixo é para reduzir linhas 
+//Fazer um código mais limpo
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jodo do número secreto')
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
