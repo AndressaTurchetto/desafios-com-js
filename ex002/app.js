ffd09bd6c1fc7a5e10dab5c4a1249eece0cb3809
Bowie <145608228+AndressaTurchetto@users.noreply.github.com>
2024-02-06 22:31:50 -0200

    Atualizando meu exercício

--- ex002/app.js
+++ ex002/app.js
@@ -1,26 +1,27 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do número secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
-//Aqui abaixo é para reduzir linhas 
-//Fazer um código mais limpo
-
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 7;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jodo do número secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+exibirTextoNaTela('h1', 'Jodo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
-}
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+    }
+} 
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
